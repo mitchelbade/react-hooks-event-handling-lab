cc59e97c5d6eb3793022e8a933c99a1cc85554fe
mitchelbade <mitchelm.bade@gmail.com>
2022-05-17 00:27:36 -0700

    commit tree cbf8a58c35555e97d3066ddbf6c0e855fd4d1fd8

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+import React, { useState } from "react"
+
+function EyesOnMe() {
+  const [focus, setFocus] = useState("");
+  const [blur, setBlur] = useState("");
+
+  function focusButton() {
+    console.log("Good!")
+  }
+
+  function blurButton() {
+    console.log("Hey! Eyes on me!")
+  }
+
+
+  return (
+    <button onFocus={focusButton} onBlur={blurButton}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React, { useState } from "react";
+
+function Keypad() {
+	const [password, setPassword] = useState("");
+
+	function input() {
+		console.log("Entering password...")
+	}
+
+  return (
+		<input type="password" value={password} onChange={input}/>
+  )
+}
+
+export default Keypad;
